56de5980ab9c6adc3b8b3ed9996d7672a272a294
DuyNT <fuujin0904@gmail.com>
2015-03-23 15:27:08 +0700

    done ganbare detail (get data, add ganbare)

--- app/js/controllers/view-detail-ganbaru-ctrl.js
+++ app/js/controllers/view-detail-ganbaru-ctrl.js
@@ -4,19 +4,43 @@
 /* jshint node: true */
 
 (function() {
-	ganbareControllers.controller('viewGanbareDetailCtrl', ['$scope', '$cookieStore', '$routeParams', 'ganbareDetail', 'addGanbare', 
-		function($scope, $cookieStore, $routeParams, ganbareDetail, addGanbare) {
+	ganbareControllers.controller('viewGanbareDetailCtrl', ['$scope', '$cookieStore', '$routeParams', '$interval', 'ganbareDetail', 'addGanbare', 
+		function($scope, $cookieStore, $routeParams, $interval, ganbareDetail, addGanbare) {
 
 		ganbareDetail.query({ganbaruId: $routeParams.ganbaruId}, function(response) {
-			console.log(response);
+			//binding data get from server
 			$scope.createdDate = response.data.ganbaru.createDate;
 			$scope.expiredDate = response.data.ganbaru.expiredDate;
 			$scope.ganbareNumber = response.data.ganbaru.ganbareNumber;
 			$scope.ganbaruContent = response.data.ganbaru.ganbaruContent;
 			$scope.username = response.data.user.username;
+
+			var clickNumber = 0;	//the number of user's ganbare clicks
+			$scope.addGanbare = function() {
+				$scope.ganbareNumber++;
+				clickNumber++;
+			};
+
+			function sendAddGanbareRequest() {
+				//if user click, then do this
+				if(clickNumber > 0) {
+					addGanbare.add({userId: $cookieStore.get('userId'), ganbareNumber: clickNumber, ganbaruId: $routeParams.ganbaruId}, 
+						function(response) {
+							console.log(response);
+						}, function() {
+							//Handle error here
+							console.log('Failed add ganbare number!');
+					});
+					clickNumber = 0;
+				}
+			};
+
+			//send Add Ganbare request to server each 3s
+			$interval(sendAddGanbareRequest, 3000);	
+
 		}, function() {
 			//Handle error here
-			console.log('Failed get ganbare');
+			console.log('Failed get ganbare detail!');
 		})
 	}]);
 })();
