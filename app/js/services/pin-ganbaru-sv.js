eabbb326d62f804ac8a4fc67e20ea6e772e683eb
hoangnv <hoangnv668@gmail.com>
2015-03-24 14:25:54 +0700

    add unpin services

--- app/js/services/pin-ganbaru-sv.js
+++ app/js/services/pin-ganbaru-sv.js
@@ -15,3 +15,16 @@ ganbareServices.factory('pinGanbaru', ['$resource',
     pin: {method: 'POST'}
   });
 }]);
+
+/*
+* Service unpin Ganbaru
+*/
+ganbareServices.factory('unPinGanbaru', ['$resource',
+  function($resource) {
+  return $resource(
+    path + 'v1/users/:userId/pins/:ganbaruId',
+    {userId: '@userId', ganbaruId: '@ganbaruId'},
+    {
+    pin: {method: 'DELETE'}
+  });
+}]);
