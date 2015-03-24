bdb23557c77bd376e31bd950f86d0fd86108d855
hoangnv <hoangnv668@gmail.com>
2015-03-24 13:55:41 +0700

    aaaaaaaaa

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,3 @@
 var path = 'http://133.242.53.250:8888/';
 
 var ganbareServices = angular.module('ganbareServices', ['ngResource']);
-
-ganbareServices.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.headers.post['Authorization'] = 'e1df2fc6-5a4a-4a9d-b55e-6702379083a8';
-}]);
