3346ac2be3e011b01eaab78241afcc333764bb29
DuyNT <fuujin0904@gmail.com>
2015-03-24 13:51:25 +0700

    pin officially

--- app/js/services.js
+++ app/js/services.js
@@ -7,6 +7,18 @@ var path = 'http://133.242.53.250:8888/';
 
 var ganbareServices = angular.module('ganbareServices', ['ngResource']);
 
+ganbareServices.factory('tokenInjector', ['$cookieStore', function($cookieStore) {
+	var tokenInjector = {
+		request: function(config) {
+			if($cookieStore.get('token')) {
+				config.headers.Authorization = $cookieStore.get('token');
+			}
+			return config;
+		}
+	};
+	return tokenInjector;
+}]);
+
 ganbareServices.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.headers.post['Authorization'] = 'db11f5f4-9e67-4728-8306-0f1feba721a3';
+	$httpProvider.interceptors.push('tokenInjector');
 }]);
