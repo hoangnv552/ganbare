e33c222a292f313cd3ff9297f1c7933811506b5f
hoangnv <hoangnv668@gmail.com>
2015-04-13 18:45:52 +0700

    add services/system-sv.js

---
+++ app/js/services/system-sv.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+	angular.module('ganbareServices').factory('System', ['ApiRootPath', '$resource', function(ApiRootPath, $resource) {
+		return $resource(ApiRootPath + 'v1/systems', {}, {
+			ping: {
+				method: 'GET'
+			}
+		});
+	}]);
+})();
